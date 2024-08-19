a7159b8d60631aa6cc5503a3d10b91b0bb6d768d
Jacob <jacobcons@gmail.com>
2024-08-19 19:50:51 +0000

    basic chat emit

---
+++ chat/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import morgan from 'morgan';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.use(morgan('tiny'));
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
